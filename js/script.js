b0c50804980c7e05120c90346ac87b071c97d96f
tutulportal <tutulvpn3@gmail.com>
2025-01-27 22:43:09 +0600

    email validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var eyeBtnOpen = document.getElementById('eye-btn-open');
 var eyeBtnClose = document.getElementById('eye-btn-close');
 var password = document.getElementById('password');
+var loginBtn = document.getElementById('login-btn');
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+var email = document.getElementById('email');
+var emailValidMessage = document.getElementById('email-valid-message');
 
 eyeBtnOpen.addEventListener('click', function(){
     eyeBtnOpen.classList.add('close-the-btn');
@@ -12,4 +16,20 @@ eyeBtnClose.addEventListener('click', function(){
     eyeBtnOpen.classList.remove('close-the-btn');
     eyeBtnClose.classList.add('close-the-btn');
     password.type = 'password';
+});
+
+loginBtn.addEventListener('click', function(e){
+    var emailInput = document.getElementById('email').value;
+    e.preventDefault();
+    if(emailPattern.test(emailInput)){
+        email.classList.remove('error-email');
+        emailValidMessage.innerText = "";
+        emailValidMessage.classList.remove('invalid');
+        emailValidMessage.classList.add('valid');
+    }else{
+        email.classList.add('error-email');
+        emailValidMessage.innerText = "This email is not correct! example: yourname@gmail.com";
+        emailValidMessage.classList.remove('valid');
+        emailValidMessage.classList.add('invalid');
+    }
 });
