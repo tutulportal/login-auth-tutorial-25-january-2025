282047e525fa1ef60df107d673836f5e405949ef
tutulportal <tutulvpn3@gmail.com>
2025-02-01 23:37:56 +0600

    full name validation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 var eyeBtnOpen = document.getElementById('eye-btn-open');
+var eyeBtnOpen2 = document.getElementById('eye-btn-open2');
 var eyeBtnClose = document.getElementById('eye-btn-close');
+var eyeBtnClose2 = document.getElementById('eye-btn-close2');
 var password = document.getElementById('password');
+var password2 = document.getElementById('confirm-password');
 var loginBtn = document.getElementById('login-btn');
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 var email = document.getElementById('email');
+var fullName = document.getElementById('full-name');
 var emailValidMessage = document.getElementById('email-valid-message');
+var fullnameValidMessage = document.getElementById('full-name-valid-message');
 
 eyeBtnOpen.addEventListener('click', function(){
     eyeBtnOpen.classList.add('close-the-btn');
     eyeBtnClose.classList.remove('close-the-btn');
     password.type = 'text';
+});
+
+eyeBtnOpen2.addEventListener('click', function(){
+    eyeBtnOpen2.classList.add('close-the-btn');
+    eyeBtnClose2.classList.remove('close-the-btn');
+    password2.type = 'text';
 });
 
 eyeBtnClose.addEventListener('click', function(){
@@ -18,9 +29,18 @@ eyeBtnClose.addEventListener('click', function(){
     password.type = 'password';
 });
 
+eyeBtnClose2.addEventListener('click', function(){
+    eyeBtnOpen2.classList.remove('close-the-btn');
+    eyeBtnClose2.classList.add('close-the-btn');
+    password2.type = 'password';
+});
+
 loginBtn.addEventListener('click', function(e){
     var emailInput = document.getElementById('email').value;
+    var fullNameInput = document.getElementById('full-name');
     e.preventDefault();
+
+    // email condition part
     if(emailPattern.test(emailInput)){
         email.classList.remove('error-email');
         emailValidMessage.innerText = "";
@@ -32,4 +52,17 @@ loginBtn.addEventListener('click', function(e){
         emailValidMessage.classList.remove('valid');
         emailValidMessage.classList.add('invalid');
     }
+
+    // full name condition part
+    if(fullNameInput.value == ""){
+        fullName.classList.add('error-full-name');
+        fullnameValidMessage.innerText = "Please Enter your full name. ex: Jhon Doe";
+        fullnameValidMessage.classList.remove('valid');
+        fullnameValidMessage.classList.add('invalid');
+    }else{
+        fullName.classList.remove('error-full-name');
+        fullnameValidMessage.classList.add('valid');
+        fullnameValidMessage.classList.remove('invalid');
+    }
+
 });
